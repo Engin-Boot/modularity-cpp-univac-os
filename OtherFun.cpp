4fd01661c629abf85530ebea7ece762a95c36746
univac <68467947+univac-os@users.noreply.github.com>
2020-08-26 12:18:16 +0530

    Create OtherFun.cpp
    .cpp

---
+++ OtherFun.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<assest.h>
+#include "TelColorCode.h"
+
+void testNumberToPair(int pairNumber,
+    TelCoColorCoder::MajorColor expectedMajor,
+    TelCoColorCoder::MinorColor expectedMinor)
+{
+    TelCoColorCoder::ColorPair colorPair =
+        TelCoColorCoder::GetColorFromPairNumber(pairNumber);
+    cout << "Got pair " << colorPair.ToString() << endl;
+    assert(colorPair.getMajor() == expectedMajor);
+    assert(colorPair.getMinor() == expectedMinor);
+}
+
+void testPairToNumber(
+    TelCoColorCoder::MajorColor major,
+    TelCoColorCoder::MinorColor minor,
+    int expectedPairNumber)
+{
+    int pairNumber = TelCoColorCoder::GetPairNumberFromColor(major, minor);
+    cout << "Got pair number " << pairNumber << endl;
+    assert(pairNumber == expectedPairNumber);
+}
