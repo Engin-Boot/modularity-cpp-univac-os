f0bf5367da91eb83a0c89329efbbc1dab2f48ff6
univac <68467947+univac-os@users.noreply.github.com>
2020-08-26 12:19:57 +0530

    Create OtherFile.h
    .h

---
+++ OtherFile.h
@@ -0,0 +1,13 @@
+ifndef OTHERFILE
+#define OTHERFILE
+
+void testNumberToPair(int pairNumber,
+    TelCoColorCoder::MajorColor expectedMajor,
+    TelCoColorCoder::MinorColor expectedMinor);
+
+void testPairToNumber(
+    TelCoColorCoder::MajorColor major,
+    TelCoColorCoder::MinorColor minor,
+    int expectedPairNumber);
+
+#endif
