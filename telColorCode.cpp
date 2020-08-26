5afdfed136dd385fae29f9b712a018cffb8b219b
univac <68467947+univac-os@users.noreply.github.com>
2020-08-26 12:07:26 +0530

    Create telColorCode.cpp
    .cpp file for .h

---
+++ telColorCode.cpp
@@ -0,0 +1,39 @@
+#include "TelColorCode.h"
+
+class  TelCocolorCoder:: ColorPair
+{
+        private:
+            MajorColor majorColor;
+            MinorColor minorColor;
+        public:
+            ColorPair(MajorColor major, MinorColor minor):
+                majorColor(major), minorColor(minor)
+            {}
+            MajorColor getMajor() {
+                return majorColor;
+            }
+            MinorColor getMinor() {
+                return minorColor;
+            }
+            string ToString() {
+                string colorPairStr = MajorColorNames[majorColor];
+                colorPairStr += " ";
+                colorPairStr += MinorColorNames[minorColor];
+                return colorPairStr;
+            }
+};
+
+void TelCoColorCoder:: ColorPair GetColorFromPairNumber(int pairNumber)
+{
+        int zeroBasedPairNumber = pairNumber - 1;
+        MajorColor majorColor = 
+            (MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+        MinorColor minorColor =
+            (MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+        return ColorPair(majorColor, minorColor);
+}
+    
+int TelCoColorCoder:: GetPairNumberFromColor(MajorColor major, MinorColor minor)
+{
+        return major * numberOfMinorColors + minor + 1;
+}
