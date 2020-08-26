73cf993895426d80f796c379483e595d19b1d1a7
univac <68467947+univac-os@users.noreply.github.com>
2020-08-26 13:08:18 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,68 @@
 #include<assert.h>
 #include "OtherFun.h"
 
+using namespace std;
+
+//defination
+class  TelCocolorCoder:: ColorPair
+{
+        private:
+            MajorColor majorColor;
+            MinorColor minorColor;
+        public:
+            ColorPair(MajorColor major, MinorColor minor):
+                majorColor(major), minorColor(minor)
+            {}
+            MajorColor getMajor() {
+                return majorColor;
+            }
+            MinorColor getMinor() {
+                return minorColor;
+            }
+            string ToString() {
+                string colorPairStr = MajorColorNames[majorColor];
+                colorPairStr += " ";
+                colorPairStr += MinorColorNames[minorColor];
+                return colorPairStr;
+            }
+};
+
+void TelCoColorCoder:: ColorPair GetColorFromPairNumber(int pairNumber)
+{
+        int zeroBasedPairNumber = pairNumber - 1;
+        MajorColor majorColor = 
+            (MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+        MinorColor minorColor =
+            (MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+        return ColorPair(majorColor, minorColor);
+}
+    
+int TelCoColorCoder:: GetPairNumberFromColor(MajorColor major, MinorColor minor)
+{
+        return major * numberOfMinorColors + minor + 1;
+}
+
+//defination
+void testNumberToPair(int pairNumber,
+    TelCoColorCoder::MajorColor expectedMajor,
+    TelCoColorCoder::MinorColor expectedMinor)
+{
+    TelCoColorCoder::ColorPair colorPair =
+        TelCoColorCoder::GetColorFromPairNumber(pairNumber);
+    cout << "Got pair " << colorPair.ToString() << endl;
+    assert(colorPair.getMajor() == expectedMajor);
+    assert(colorPair.getMinor() == expectedMinor);
+}
+
+void testPairToNumber(
+    TelCoColorCoder::MajorColor major,
+    TelCoColorCoder::MinorColor minor,
+    int expectedPairNumber)
+{
+    int pairNumber = TelCoColorCoder::GetPairNumberFromColor(major, minor);
+    cout << "Got pair number " << pairNumber << endl;
+    assert(pairNumber == expectedPairNumber);
+}
 int main() 
 {
     testNumberToPair(4, TelCoColorCoder::WHITE, TelCoColorCoder::BROWN);
