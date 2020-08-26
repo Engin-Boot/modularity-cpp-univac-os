73181b1c0301aea0c296ffde578570e946aec144
univac <68467947+univac-os@users.noreply.github.com>
2020-08-26 11:54:01 +0530

    Update telColorCode.h

--- telColorCode.h
+++ telColorCode.h
@@ -35,6 +35,6 @@ namespace TelCoColorCoder
     };
 
     ColorPair GetColorFromPairNumber(int pairNumber); 
-    int GetPairNumberFromColor(MajorColor major, MinorColor minor) 
+    int GetPairNumberFromColor(MajorColor major, MinorColor minor); 
 }
 #endif
