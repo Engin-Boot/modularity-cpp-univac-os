1e506e898c2e549af94926af08907cb1e58bc639
univac <68467947+univac-os@users.noreply.github.com>
2020-08-26 12:28:46 +0530

    Update main.cpp
    .cpp made it

--- main.cpp
+++ main.cpp
@@ -1,29 +1,10 @@
 #include<iostream>
 #include<assert.h>
 #include "telColorCode.h"
+#include "OtherFun.h"
 
-void testNumberToPair(int pairNumber,
-    TelCoColorCoder::MajorColor expectedMajor,
-    TelCoColorCoder::MinorColor expectedMinor)
+int main() 
 {
-    TelCoColorCoder::ColorPair colorPair =
-        TelCoColorCoder::GetColorFromPairNumber(pairNumber);
-    cout << "Got pair " << colorPair.ToString() << endl;
-    assert(colorPair.getMajor() == expectedMajor);
-    assert(colorPair.getMinor() == expectedMinor);
-}
-
-void testPairToNumber(
-    TelCoColorCoder::MajorColor major,
-    TelCoColorCoder::MinorColor minor,
-    int expectedPairNumber)
-{
-    int pairNumber = TelCoColorCoder::GetPairNumberFromColor(major, minor);
-    cout << "Got pair number " << pairNumber << endl;
-    assert(pairNumber == expectedPairNumber);
-}
-
-int main() {
     testNumberToPair(4, TelCoColorCoder::WHITE, TelCoColorCoder::BROWN);
     testNumberToPair(5, TelCoColorCoder::WHITE, TelCoColorCoder::SLATE);
 
