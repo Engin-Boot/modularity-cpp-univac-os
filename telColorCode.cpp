4af9f9b623606a545fff374b6e787c19c61d44c6
univac <68467947+univac-os@users.noreply.github.com>
2020-08-27 11:39:04 +0530

    Update telColorCode.cpp

--- telColorCode.cpp
+++ telColorCode.cpp
@@ -1,40 +1,18 @@
-#include "telColorCode.h"
-using namespace TelCocolorCoder
-//defination
-class  TelCocolorCoder:: ColorPair
-{
-        private:
-            MajorColor majorColor;
-            MinorColor minorColor;
-        public:
-            ColorPair(MajorColor major, MinorColor minor):
-                majorColor(major), minorColor(minor)
-            {}
-            MajorColor getMajor() {
-                return majorColor;
-            }
-            MinorColor getMinor() {
-                return minorColor;
-            }
-            string ToString() {
-                string colorPairStr = MajorColorNames[majorColor];
-                colorPairStr += " ";
-                colorPairStr += MinorColorNames[minorColor];
-                return colorPairStr;
-            }
-};
-
-void TelCoColorCoder:: ColorPair GetColorFromPairNumber(int pairNumber)
-{
-        int zeroBasedPairNumber = pairNumber - 1;
-        MajorColor majorColor = 
-            (MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
-        MinorColor minorColor =
-            (MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
-        return ColorPair(majorColor, minorColor);
-}
-    
-int TelCoColorCoder:: GetPairNumberFromColor(MajorColor major, MinorColor minor)
-{
-        return major * numberOfMinorColors + minor + 1;
+#include "pairclass.h"
+#include<string.h>
+//defination of class
+namespace TelCoColorCoder {
+     
+    MajorColor ColorPair::getMajor() {
+        return majorColor;
+    }
+    MinorColor ColorPair::getMinor() {
+        return minorColor;
+    }
+    std::string ColorPair::ToString() {
+        std::string colorPairStr = MajorColorNames[majorColor];
+        colorPairStr += " ";
+        colorPairStr += MinorColorNames[minorColor];
+        return colorPairStr;
+    }
 }
