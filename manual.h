3016f32b099563bc2480e883ded9c9b35c02b451
univac <68467947+univac-os@users.noreply.github.com>
2020-08-27 16:58:58 +0530

    Create manual.h

---
+++ manual.h
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#include "TestPair.h"
+#include<string.h>
+
+string Manual_print(int numberOfMajorColors, int numberOfMinorColors){
+	int out=0, in=0, pairNumber = 1;
+	vector<string> pairs;
+	string all ="";
+	for(out=0; out<numberOfMajorColors; out++){
+	    for(in=0; in<numberOfMinorColors; in++){ 
+		    TelCoColorCoder::ColorPair colorPair = TelCoColorCoder::GetColorFromPairNumber(pairNumber);
+        	ostringstream ss;
+        	ss << pairNumber;
+        	string buffer=ss.str()+" -- "+colorPair.ToString();
+        	pairs.push_back(buffer);
+        	pairs.push_back("\n");
+   	        pairNumber++;
+	    }
+    }  //adding all pairs  
+	for (int i = 0; i < pairs.size(); i++) {
+            all += pairs[i];
+	}
+	return all;
+}
