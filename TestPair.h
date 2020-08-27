861294837a9877edeeedff2fdb9698b8f6e73625
univac <68467947+univac-os@users.noreply.github.com>
2020-08-27 11:52:36 +0530

    Create TestPair.h

---
+++ TestPair.h
@@ -0,0 +1,9 @@
+#include "getColor.h"
+
+void testNumberToPair(int pairNumber,
+    TelCoColorCoder::MajorColor expectedMajor,
+    TelCoColorCoder::MinorColor expectedMinor);
+void testPairToNumber(
+    TelCoColorCoder::MajorColor major,
+    TelCoColorCoder::MinorColor minor,
+    int expectedPairNumber);
