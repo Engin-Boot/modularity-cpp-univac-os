6fe1a4bcb79a6ee31f0d0645ff029ca86aa35ec0
univac <68467947+univac-os@users.noreply.github.com>
2020-08-27 12:04:32 +0530

    Update TestPair.cpp

--- TestPair.cpp
+++ TestPair.cpp
@@ -1,5 +1,6 @@
 #include<assert.h>
 #include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 #include "TestPair.h"
 //defination
@@ -22,4 +23,25 @@ void testPairToNumber(
     int pairNumber = TelCoColorCoder::GetPairNumberFromColor(major, minor);
     cout << "Got pair number " << pairNumber << endl;
     assert(pairNumber == expectedPairNumber);
+}
+
+string Manual_print(int numberOfMajorColors, int numberOfMinorColors){
+	int out=0, in=0, pairNumber = 1;
+	vector<string> pairs;
+	string all ="";
+	for(out=0; out<numberOfMajorColors; out++){
+	    for(in=0; in<numberOfMinorColors; in++){ 
+		    TelCoColorCoder::ColorPair colorPair = TelCoColorCoder::GetColorFromPairNumber(pairNumber);
+        	ostringstream ss;
+        	ss << pairNumber;
+        	string buffer=ss.str()+" -- "+colorPair.ToString();
+        	pairs.push_back(buffer);
+        	pairs.push_back("\n");
+   	        pairNumber++;
+	    }
+}  //adding all pairs  
+	for (int i = 0; i < pairs.size(); i++) {
+            all += pairs[i];
+	}
+	return all;
 }
