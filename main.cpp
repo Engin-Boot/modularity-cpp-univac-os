baf93f6feabb517a533b0b139a1c218b9b5423c9
univac <68467947+univac-os@users.noreply.github.com>
2020-08-27 16:52:38 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,26 @@
 #include <assert.h>
 
 #include "TestPair.h"
-
+string Manual_print(int numberOfMajorColors, int numberOfMinorColors){
+	int out=0, in=0, pairNumber = 1;
+	vector<string> pairs;
+	string all ="";
+	for(out=0; out<numberOfMajorColors; out++){
+	    for(in=0; in<numberOfMinorColors; in++){ 
+		    TelCoColorCoder::ColorPair colorPair = TelCoColorCoder::GetColorFromPairNumber(pairNumber);
+        	ostringstream ss;
+        	ss << pairNumber;
+        	string buffer=ss.str()+" -- "+colorPair.ToString();
+        	pairs.push_back(buffer);
+        	pairs.push_back("\n");
+   	        pairNumber++;
+	    }
+    }  //adding all pairs  
+	for (int i = 0; i < pairs.size(); i++) {
+            all += pairs[i];
+	}
+	return all;
+}
 int main() {
     testNumberToPair(4, TelCoColorCoder::WHITE, TelCoColorCoder::BROWN);
     testNumberToPair(5, TelCoColorCoder::WHITE, TelCoColorCoder::SLATE);
