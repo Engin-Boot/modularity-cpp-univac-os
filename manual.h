f496dd7ccb57021d52721f5b60a7c02939204c14
univac <68467947+univac-os@users.noreply.github.com>
2020-08-27 17:24:31 +0530

    Update manual.h

--- manual.h
+++ manual.h
@@ -2,5 +2,5 @@
 #include "TestPair.h"
 #include<string.h>
 
-void Manual_pairing(int numberOfMajorColors, int numberOfMinorColors);	  
 string Manual_print(vector<string> pairs,int size);
+void Manual_pairing(int numberOfMajorColors, int numberOfMinorColors);	  
